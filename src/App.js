673a5e8daa526bbe50ed60de93e35a5a124feda6
Panyakorn Boonyong <panyakorn40@gmail.com>
2021-10-20 10:32:26 +0700

    create pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import Homepage from "./Component/Homepage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home";
+import About from "./pages/AboutPage";
+import Contact from "./pages/ContactPage";
+import Signin from "./pages/SigninPage";
+import Signup from "./pages/SignupPage";
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
