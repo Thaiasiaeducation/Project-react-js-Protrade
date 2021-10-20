4ac28440a8b045a8a3b01576673a5b0a98de7d2d
Panyakorn Boonyong <panyakorn40@gmail.com>
2021-10-20 13:26:30 +0700

    create contact

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Contact from "../Component/Contact/Contact";
 
 function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
 }
 
 export default ContactPage;
